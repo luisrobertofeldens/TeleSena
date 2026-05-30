30234290b8710cc96da779ac522f58605aa8e313
Luis Roberto Feldens <luisrobertofeldens@gmail.com>
2026-05-29 23:38:26 -0300

    Recriação de "TeleSena" tornando-a funcional.

---
+++ src/Testes.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Testes {
+    public static void main(String[] args) {
+        TeleSena t = new TeleSena();
+
+        System.out.println(Arrays.toString(t.getNumeros1()));
+        System.out.println(Arrays.toString(t.getNumeros2()));
+    }
+}
